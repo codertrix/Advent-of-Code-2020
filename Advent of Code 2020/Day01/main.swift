3f90253392f2384a6469f6c426edee00784a65d4
Michael Fenske <michael@codertrix.com>
2020-12-01 16:52:52 +0100

    Day 1: Report Repair

---
+++ Advent of Code 2020/Day01/main.swift
@@ -0,0 +1,46 @@
+// ---------------------------------------------------
+//  main.swift
+//  Day01: Report Repair
+//
+//  Copyright © 2020 Michael Fenske. See LICENSE.txt.
+// ---------------------------------------------------
+
+import Foundation
+
+
+let inputCount = input.count
+
+
+// --------------------
+//   MARK: - Puzzle 1
+// --------------------
+
+puzzleOne:
+for idx1 in 1..<inputCount {
+    let value = input[idx1]
+    for idx2 in idx1..<inputCount {
+        if value + input[idx2] == 2020 {
+            print("Result for puzzle 1: \(value * input[idx2])")
+            break puzzleOne
+        }
+    }
+}
+
+
+// --------------------
+//   MARK: - Puzzle 2
+// --------------------
+
+puzzleTwo:
+for idx1 in 1..<inputCount {
+    let value1 = input[idx1]
+    for idx2 in idx1..<inputCount {
+        let value2 = input[idx2]
+        for idx3 in idx2..<inputCount {
+            if value1 + value2 + input[idx3] == 2020 {
+                print("Result for puzzle 2: \(value1 * value2 * input[idx3])")
+                break puzzleTwo
+            }
+        }
+    }
+}
